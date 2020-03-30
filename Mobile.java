ec25d6153c317e647aba5644e1dd48c71fdcc906
Harikrishna58 <62900886+Harikrishna58@users.noreply.github.com>
2020-03-30 22:44:56 +0530

    Update and rename README.md to Mobile.java

---
+++ Mobile.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Mobile {
+boolean isNew=true;
+int number=1005;
+String name="Nokia";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Mobile mob=new Mobile();
+		boolean blue2 = mob.isNew;
+System.out.println(blue2);
+int num2 = mob.number;
+System.out.println(num2);
+String name2 = mob.name;
+System.out.println(name2);
+	}
+
+}
