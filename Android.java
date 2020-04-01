e9139dcd200976414b16c1e444ed969fe30f82bd
Harikrishna58 <62900886+Harikrishna58@users.noreply.github.com>
2020-04-01 16:41:38 +0530

    Add files via upload

---
+++ Android.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class Android {
+
+	long num=985632417;
+	boolean isFast=true;
+	String model="MotoG5S+";
+	int phAge=5;
+	float pixel=8.5f;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+Android and=new Android();
+ long num2=and.num;
+      System.out.println(num2);
+ boolean isFast2=and.isFast;
+	System.out.println(isFast2);
+String model2=and.model;
+	System.out.println(model2);
+int phAge2=and.phAge;
+	System.out.println(phAge2);
+float pixel2=and.pixel;
+	System.out.println(pixel2);
+ 
+	}
+
+}
